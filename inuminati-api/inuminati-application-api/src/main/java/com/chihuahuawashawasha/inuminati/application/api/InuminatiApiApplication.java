0d73f649b20b4befafbf6c45303a3d54e2736a71
k21hornet <knt0535.tk@gmail.com>
2025-12-04 23:11:37 +0900

    feat: sub modules

--- inuminati-api/inuminati-application-api/src/main/java/com/chihuahuawashawasha/inuminati/application/api/InuminatiApiApplication.java
+++ inuminati-api/inuminati-application-api/src/main/java/com/chihuahuawashawasha/inuminati/application/api/InuminatiApiApplication.java
@@ -1,4 +1,4 @@
-package com.chihuahuawashawasha.inuminati;
+package com.chihuahuawashawasha.inuminati.application.api;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
--- inuminati-api/inuminati-application-api/src/main/java/com/chihuahuawashawasha/inuminati/application/api/controller/HealthController.java
+++ inuminati-api/inuminati-application-api/src/main/java/com/chihuahuawashawasha/inuminati/application/api/controller/HealthController.java
@@ -1,4 +1,4 @@
-package com.chihuahuawashawasha.inuminati.controller;
+package com.chihuahuawashawasha.inuminati.application.api.controller;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
